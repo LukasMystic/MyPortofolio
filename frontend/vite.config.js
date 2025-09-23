388aa7fd8bb4c775e2947a38a3e0fa386a91b297
LukasMystic <112644367+LukasMystic@users.noreply.github.com>
2025-09-23 15:37:42 +0700

    bug fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://portofoliobackend.vercel.app/', 
+        target: 'https://portofoliobackend.vercel.app', 
         changeOrigin: true,
       },
     },
